d4a1a07381db6b967673c1b5da02dd49927fd248
Ethan <ethanwheeler@invenshure.com>
2022-09-25 21:12:06 -0500

    Changes to auth.server

--- app/utils/auth.server.ts
+++ app/utils/auth.server.ts
@@ -4,6 +4,23 @@ import { json, createCookieSessionStorage, redirect } from "@remix-run/node";
 import { createUser } from "./user.server";
 import bcrypt from "bcryptjs";
 
+// COOKIE STORAGE STUFF
+const sessionSecret = process.env.SESSION_SECRET;
+if (!sessionSecret) {
+  throw new Error("SESSION_SECRET must be set");
+}
+const storage = createCookieSessionStorage({
+  cookie: {
+    name: "secrets-session",
+    secure: process.env.NODE_ENV === "production",
+    secrets: [sessionSecret],
+    sameSite: "lax",
+    path: "/",
+    maxAge: 60 * 60 * 24 * 30,
+    httpOnly: true,
+  },
+});
+
 // Pulled From: https://www.prisma.io/blog/fullstack-remix-prisma-mongodb-2-ZTmOy58p4re8#create-an-instance-of-prisma
 export async function register(user: RegisterForm) {
   const exists = await prisma.user.count({ where: { email: user.email } });
@@ -27,9 +44,15 @@ export async function register(user: RegisterForm) {
   return createUserSession(newUser.id, "/");
 }
 
+function getUserSession(request: Request) {
+  return storage.getSession(request.headers.get("Cookie"));
+}
+
 export async function createUserSession(userId: number, redirectTo: string) {
   const session = await storage.getSession();
+
   session.set("userId", userId);
+  console.log(session.get("userId"));
   return redirect(redirectTo, {
     headers: {
       "Set-Cookie": await storage.commitSession(session),
@@ -37,28 +60,41 @@ export async function createUserSession(userId: number, redirectTo: string) {
   });
 }
 
+export async function logout(request: Request) {
+  const session = await getUserSession(request);
+  return redirect("/login", {
+    headers: {
+      "Set-Cookie": await storage.destroySession(session),
+    },
+  });
+}
+
+export async function getUserId(request: Request) {
+  const session = await getUserSession(request);
+  const userId = session.get("userId");
+  if (!userId || typeof userId !== "number") return null;
+  return userId;
+}
+
+export async function getUser(request: Request) {
+  const userId = await getUserId(request);
+  if (typeof userId !== "number") {
+    return null;
+  }
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+    return user;
+  } catch {
+    throw logout(request);
+  }
+}
 export async function login({ email, password }: LoginForm) {
   const user = await prisma.user.findUnique({ where: { email } });
 
   if (!user || !(await bcrypt.compare(password, user.password))) {
     return json({ error: `Incorrect login` }, { status: 400 });
   }
-  return createUserSession(user.id, "/home");
+  return createUserSession(user.id, "/passwords");
 }
-
-// COOKIE STORAGE STUFF
-const sessionSecret = process.env.SESSION_SECRET;
-if (!sessionSecret) {
-  throw new Error("SESSION_SECRET must be set");
-}
-const storage = createCookieSessionStorage({
-  cookie: {
-    name: "secrets-session",
-    secure: process.env.NODE_ENV === "production",
-    secrets: [sessionSecret],
-    sameSite: "lax",
-    path: "/",
-    maxAge: 60 * 60 * 24 * 30,
-    httpOnly: true,
-  },
-});
